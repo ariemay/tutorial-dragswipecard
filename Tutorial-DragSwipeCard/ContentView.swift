81c3a5aa32c7d158b3b11a2f3f96e18ab5959c2b
Arie May Wibowo <ariemayw@gmail.com>
2020-08-11 17:20:57 +0700

    Initial Commit

---
+++ Tutorial-DragSwipeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial-DragSwipeCard
+//
+//  Created by Arie May Wibowo on 11/08/20.
+//  Copyright © 2020 Arie May Wibowo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
